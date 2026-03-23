9c56aed6105ff74e777e48937c2b4c01619acd22
MRHMisu <mdrh@uci.edu>
2026-03-23 13:12:02 -0700

    uodate optimizer run script

--- optimizer/run_optimizer.sh
+++ optimizer/run_optimizer.sh
@@ -2,6 +2,7 @@ python -m optimizer.prompt_optimizer \
   --formalbench_path /home/mdrh/code/specsyns/benchmarks/formalbench/fb.json\
   --specgenbench_path /home/mdrh/code/specsyns/benchmarks/specgenbench/sgb.json \
   --optimizers gepa \
-  -- best_seed specgen \
-  --log_dir /home/mdrh/experiments/output_optimizer/optimizer_logs \
-  --output_dir /home/mdrh/experiments/output_optimizer/optimizer_results
+  -- best_seed zero \
+  --log_dir /home/mdrh/experiments/output_optimizer/optimizer_logs_zero \
+  --output_dir /home/mdrh/experiments/output_optimizer/optimizer_results_zero \
+  --openjml_output_dir /home/mdrh/experiments/output_optimizer/openjml_output_zero
